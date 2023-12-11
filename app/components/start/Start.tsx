5354849de504472bed7dd40428682216d4df5896
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:14:14 -0800

    Create Start.tsx

---
+++ app/components/start/Start.tsx
@@ -0,0 +1,13 @@
+export default function Start({
+}) {
+  return (
+    <div className="relative z-10 flex flex-col items-center justify-center w-full h-full bg-neutral-200 text-center overflow-clip">
+      <h1 className="text-purple-100 font-karla text-[31.25px] leading-[36.53px] ">
+        Quizzical
+      </h1>
+      <p className="mt-[11px] mb-[29px] text-purple-100 leading-5">
+        How well do you know your trivia?
+      </p>
+    </div>
+  );
+}
