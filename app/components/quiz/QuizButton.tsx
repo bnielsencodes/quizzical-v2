965f063be134cc524d9e4ff98b1619c5126a5e25
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:29:52 -0800

    Create QuizButton.tsx

---
+++ app/components/quiz/QuizButton.tsx
@@ -0,0 +1,9 @@
+export default function QuizButton({
+}) {
+  return (
+    <button
+      className="check check-answers--btn py-[11px] border-0 rounded-[10px] bg-purple-200 text-neutral-200 text-[13px] leading-[12.39px] font-semibold shadow-custom md:py-4 md:px-[23px] md:text-[14px]"
+    >
+    </button>
+  );
+}
