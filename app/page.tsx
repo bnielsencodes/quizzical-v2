4ee941af4394da05ddf8f33f98ba4f449ebaf04e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:26:56 -0800

    Update styles

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="relative grid place-items-center h-[100svh] bg-neutral-100">
         <Start {...{ gameOptions, handleChange, startGame }} />
         />
       <Footer />
