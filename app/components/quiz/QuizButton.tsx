35d97df0e29828c1bf8577152ecb527ca87f2dc8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:39:46 -0800

    Conditionally render text based on answersChecked value

--- app/components/quiz/QuizButton.tsx
+++ app/components/quiz/QuizButton.tsx
@@ -11,6 +11,7 @@ export default function QuizButton({
     <button
       className="check check-answers--btn py-[11px] border-0 rounded-[10px] bg-purple-200 text-neutral-200 text-[13px] leading-[12.39px] font-semibold shadow-custom md:py-4 md:px-[23px] md:text-[14px]"
     >
+      {answersChecked ? "Play Again" : "Check Answers"}
     </button>
   );
 }
