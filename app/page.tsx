af3066a5b3e52a51200711875254eaf995b1149d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:43:57 -0800

    Show how many correct answers

--- app/page.tsx
+++ app/page.tsx
@@ -109,6 +109,7 @@ export default function Home() {
           {...{
             checkAnswers,
             answersChecked,
+            answersCorrect,
             playAgain,
           }}
         />
