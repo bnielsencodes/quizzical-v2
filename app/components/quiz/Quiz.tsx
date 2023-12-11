cc48c38cc56a50d06336b9b656144d3d263d6a75
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:28:29 -0800

    Create Quiz.tsx

---
+++ app/components/quiz/Quiz.tsx
@@ -0,0 +1,7 @@
+export default function Quiz({
+}) {
+  return (
+    <div className="relative flex flex-col items-center content-center w-screen min-h-screen pt-[60px] px-10 pb-[15px] bg-neutral-200 md:pt-[13px] md:px-[75px]">
+    </div>
+  );
+}
