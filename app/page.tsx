529080de212ac8facb078030373fa743b8a3eeb0
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:09:49 -0800

    Add object interfaces

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 import Image from 'next/image'
+"use client";
 
+
+interface GameOptions {
+  category: string;
+  difficulty: string;
+  type: string;
+}
+
+interface Question {
+  id: string;
+  question: string;
+  answers: string[];
+  correct: string;
+  selected: string | null;
+  checked: boolean;
+}
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
