13f2438e5f8ba94897fd6d546b36c580396b8be8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:23:02 -0800

    Add event listener

--- app/components/start/StartButton.tsx
+++ app/components/start/StartButton.tsx
@@ -2,6 +2,7 @@ export default function StartButton({ startGame }: { startGame: () => void }) {
   return (
     <button
       className="btn primary py-4 px-14 shadow-custom"
+      onClick={startGame}
     >
       Start Quiz
     </button>
