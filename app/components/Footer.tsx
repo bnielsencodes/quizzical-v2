59b1cba8809c4fe728fdad337b5e054af7627c52
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:23:15 -0800

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <p className="footer absolute bottom-[25px] left-0 w-full font-karla text-[14px] font-bold text-center">
+      Made by{" "}
+      <a
+        className="underlineAnimation text-purple-200 no-underline"
+        href="https://bnielsen.dev"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Brandon Nielsen
+      </a>
+    </p>
+  );
+}
