33659403c226d1243fc3914f66cdfc5d15199b22
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:45:23 -0800

    Pass props, map questions state, and import and render questionElements

--- app/components/quiz/Quiz.tsx
+++ app/components/quiz/Quiz.tsx
@@ -1,18 +1,32 @@
+import QAndA from "./QAndA";
 import QuizButton from "./QuizButton";
 
 export default function Quiz({
+  questions,
+  selectAnswer,
   checkAnswers,
   answersChecked,
   answersCorrect,
   playAgain,
 }: {
+  questions: any;
+  selectAnswer: (id: string, answer: string) => void;
   checkAnswers: () => void;
   answersChecked: boolean;
   answersCorrect: number;
   playAgain: () => void;
 }) {
+  // map questions state and create QAndA components
+  const questionElements = questions
+    ? questions.map((item: any) => {
+        console.log(item);
+        return <QAndA key={item.id} {...{ item, selectAnswer }} />;
+      })
+    : [];
+
   return (
     <div className="relative flex flex-col items-center content-center w-screen min-h-screen pt-[60px] px-10 pb-[15px] bg-neutral-200 md:pt-[13px] md:px-[75px]">
+      <div className="z-20 mb-[5px] md:max-w-[750px]">{questionElements}</div>
       <div className="z-20 flex items-center justify-around w-[93%] max-w-[450px] mb-[50px]">
         {answersChecked && (
           <span className="text-purple-100 font-bold text-[13px] md:text-[1rem]">
--- app/page.tsx
+++ app/page.tsx
@@ -107,6 +107,8 @@ export default function Home() {
       ) : (
         <Quiz
           {...{
+            questions,
+            selectAnswer,
             checkAnswers,
             answersChecked,
             answersCorrect,
