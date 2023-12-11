f59c50717ef041f1ddadb0f9f284d78bf1ae333d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:20:04 -0800

    Import GameOptions.tsx and pass props

--- app/components/start/Start.tsx
+++ app/components/start/Start.tsx
@@ -1,4 +1,15 @@
+import GameOptions from "./GameOptions";
+
 export default function Start({
+  gameOptions,
+  handleChange,
+}: {
+  gameOptions: {
+    category: string;
+    difficulty: string;
+    type: string;
+  };
+  handleChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }) {
   return (
     <div className="relative z-10 flex flex-col items-center justify-center w-full h-full bg-neutral-200 text-center overflow-clip">
@@ -8,6 +19,7 @@ export default function Start({
       <p className="mt-[11px] mb-[29px] text-purple-100 leading-5">
         How well do you know your trivia?
       </p>
+      <GameOptions {...{ gameOptions, handleChange }} />
     </div>
   );
 }
