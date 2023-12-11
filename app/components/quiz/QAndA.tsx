0f6a2e6524d124b5b467829e616fb87855606cb6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:46:52 -0800

    Create answers variable and handleSelectAnswer function

--- app/components/quiz/QAndA.tsx
+++ app/components/quiz/QAndA.tsx
@@ -1,5 +1,18 @@
 export default function QAndA({
+  selectAnswer,
+}: {
+  };
+  selectAnswer: (id: string, answer: string) => void;
 }) {
+  let answers: string[] = item.answers;
+
+  function handleSelectAnswer(answer: string): void {
+    if (item.checked) {
+      return;
+    }
+    selectAnswer(item.id, answer);
+  }
+
   return (
     <div className="flex justify-between border-b-[0.79px] border-purple-300 mb-[15px]">
     </div>
