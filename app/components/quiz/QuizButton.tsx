725aa54f1386cc59446247465ffb4ac004943b3f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 13:39:54 -0800

    Add event listener

--- app/components/quiz/QuizButton.tsx
+++ app/components/quiz/QuizButton.tsx
@@ -10,6 +10,7 @@ export default function QuizButton({
   return (
     <button
       className="check check-answers--btn py-[11px] border-0 rounded-[10px] bg-purple-200 text-neutral-200 text-[13px] leading-[12.39px] font-semibold shadow-custom md:py-4 md:px-[23px] md:text-[14px]"
+      onClick={answersChecked ? playAgain : checkAnswers}
     >
       {answersChecked ? "Play Again" : "Check Answers"}
     </button>
