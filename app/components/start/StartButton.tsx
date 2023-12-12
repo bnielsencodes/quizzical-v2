a881c5a167fd1af0fca5f5e452a760993a109903
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 23:34:48 -0800

    Update button styles and effects

--- app/components/start/StartButton.tsx
+++ app/components/start/StartButton.tsx
@@ -1,7 +1,7 @@
 export default function StartButton({ startGame }: { startGame: () => void }) {
   return (
     <button
-      className="btn primary py-4 px-14 shadow-custom"
+      className="customTransition py-4 px-14 border-0 rounded-[15px] bg-purple-200 text-neutral-200 font-medium leading-5 shadow-btn active:translate-y-[3.5px] active:shadow-none lg:hover:bg-opacity-95"
       onClick={startGame}
     >
       Start Quiz
