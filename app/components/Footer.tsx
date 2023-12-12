20615ff94a37c8fadb2b2d4c25947c88c3c2545e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-11 23:32:32 -0800

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,15 +1,15 @@
 export default function Footer() {
   return (
-    <p className="footer absolute bottom-[25px] left-0 w-full font-karla text-[14px] font-bold text-center">
+    <footer className="absolute bottom-[25px] left-0 z-20 w-full font-karla text-[14px] font-bold text-center">
       Made by{" "}
       <a
-        className="underlineAnimation text-purple-200 no-underline"
+        className="underlineAnimation relative text-purple-200 no-underline"
         href="https://bnielsen.dev"
         target="_blank"
         rel="noreferrer"
       >
         Brandon Nielsen
       </a>
-    </p>
+    </footer>
   );
 }
