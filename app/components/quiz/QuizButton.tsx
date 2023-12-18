93fcd6f5ce72cc7a76de7497beb3fe6c2810edca
Brandon Nielsen <bnielsencodes@gmail.com>
2023-12-18 02:11:36 -0800

    Update shadow

--- app/components/quiz/QuizButton.tsx
+++ app/components/quiz/QuizButton.tsx
@@ -9,7 +9,7 @@ export default function QuizButton({
 }) {
   return (
     <button
-      className="customTransition py-4 px-7 border-0 rounded-[15px] bg-purple-200 text-neutral-200 font-medium leading-5 shadow-btn active:translate-y-[3.5px] active:shadow-none lg:hover:bg-opacity-95"
+      className="customTransition py-4 px-7 border-0 rounded-[15px] bg-purple-200 text-neutral-200 font-medium leading-5 shadow-btn active:translate-y-[4.5px] active:shadow-none lg:hover:bg-opacity-95"
       onClick={answersChecked ? playAgain : checkAnswers}
     >
       {answersChecked ? "Play Again" : "Check Answers"}
